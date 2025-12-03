9c409c444440f2af8472a53308fb4ae245bb14d7
samuel.beard <samuel.beard.1@us.af.mil>
2025-12-03 16:03:17 -0500

    added additional memebers to user model on backend

--- internal/models/user.go
+++ internal/models/user.go
@@ -2,6 +2,7 @@ package models
 
 type User struct {
 	UserID Identity `json:"userID"`
+	UserData Data   `json:"data"`
 }
 
 type Identity struct {
@@ -9,3 +10,16 @@ type Identity struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
 }
+
+type Data struct {
+	Files       []FileData 	`json:"files"`
+	Preferences Preferences `json:"preferences"`
+}
+
+type FileData struct { 
+	Name string `json:"fileName"`
+	Body []byte `json:"fileBody"`
+}
+
+type Preferences struct { 
+} 
