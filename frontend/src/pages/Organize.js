0a2e2520eb0049a9368c286b4924c29da5a67ac6
samuel.beard <samuel.beard.1@us.af.mil>
2025-12-03 14:08:20 -0500

    added input for file upload on organize page

--- frontend/src/pages/Organize.js
+++ frontend/src/pages/Organize.js
@@ -13,9 +13,29 @@ export default function Organize(root, userData) {
 				${username}
 			</h1>
 
-			<div id='upload-files'>
-				<!-- Need to decide what user data we're maintaining in database if using with an account -->
+			<div  id='upload-form-container'>
+				<!-- Need to decide what user data we're maintaining in database if using with an account 
 				
+				link to resource: https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications'
+				
+				drag and drop: https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
+'
+				<form name='upload-form'>
+					<div>
+						<input id="upload-input" type='file' multiple />
+					</div>
+				</form>
+				-->
+				<label  style='border: 0.05em white solid' id='drop-zone'>
+					Drop files here, or click to upload.
+					<input style="display: none;" id='file-input' type='file' multiple />
+				</label>
+				<!-- 
+				Here is where we will display the preview for all files 
+				uploaded so far
+				-->
+				<ul id='file-preview'></ul>
+
 			</div>
 		</div>
 
