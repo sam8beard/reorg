8a948bc44eb1b97764587d4d65697651f47f1f82
sam8beard <sammybeard2002@gmail.com>
2025-12-10 11:51:06 -0500

    modified init script for db

--- internal/db/db-init/create_tables.sql
+++ internal/db/db-init/create_tables.sql
@@ -1,6 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE users ( 
 	id SERIAL PRIMARY KEY,
-	username TEXT NOT NULL, 
+	username TEXT UNIQUE NOT NULL, 
 	password_hash TEXT UNIQUE NOT NULL,
 	email TEXT UNIQUE NOT NULL,
 	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
@@ -8,6 +10,37 @@ CREATE TABLE users (
 
 CREATE TABLE uploads (
 	id SERIAL PRIMARY KEY,
-	user_id INTEGER REFERENCES users(id), --nullable for guests--
-	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()	
+	upload_uuid UUID NOT NULL DEFAULT gen_random_uuid(),
+	user_id INTEGER REFERENCES users(id) NULL, --nullable for guests--
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE files (
+	id SERIAL PRIMARY KEY,
+	upload_id INTEGER REFERENCES uploads(id),
+	s3_key TEXT NOT NULL,
+	original_path TEXT NOT NULL,
+	size INTEGER NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE rulesets (
+	id SERIAL PRIMARY KEY,
+	ruleset_uuid UUID NOT NULL DEFAULT gen_random_uuid(),
+	user_id INTEGER REFERENCES users(id) NULL, --nullable for guests--
+	name TEXT NOT NULL, 
+	rules_json JSONB NOT NUll,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE jobs (
+	id SERIAL PRIMARY KEY,
+	job_uuid UUID NOT NULL DEFAULT gen_random_uuid(),
+	user_id INTEGER REFERENCES users(id) NULL, --nullable for guests--
+	upload_id INTEGER REFERENCES uploads(id),
+	ruleset_json JSONB NOT NULL,
+	status TEXT NOT NULL,
+	result_path TEXT,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ DEFAULT NOW()
 );
