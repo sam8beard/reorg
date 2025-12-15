59dfdbcb9be9108ac284f988e069d168c14fe60a
sam8beard <sammybeard2002@gmail.com>
2025-12-14 21:00:33 -0500

    added dummy input on rule creation page for file type selection and name matching

--- frontend/src/pages/RuleCreation.js
+++ frontend/src/pages/RuleCreation.js
@@ -7,7 +7,22 @@ export default function RuleCreation(root, user) {
 			<!-- Is this the optimal way to get the most recently added target dir??? -->
 			<h1> Add rules for ${currTarget.targetName} </h1>
 			<div id='rule-option-container'>
-				
+				<!-- Implement some external module for the multiselect dropdown -->
+				<form id='rule-option-form' method='post'>
+					<div>
+						<label for='fileTypes'>File Type(s): </label>
+						<select name='fileTypes' id='fileTypes' multiple data-multi-select>
+							<option value="pdf">PDF (.pdf)</option>
+							<option value="image">Images (.jpg, .png)</option>
+							<option value="video">Videos (.mp3, .mov)</option>
+							<option value="text">Text files (.txt, .md)</option>
+						</select>
+					</div>
+					<div>
+						<label for='nameContains'>Name Contains: </label>
+						<input name='nameContains' id='nameContains'>
+					</div>
+				</form>
 			</div>
 		</div>
 	`;
